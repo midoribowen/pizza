59b4a01f0e3bb3b8da2c2462d91831a7c1e12250
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-12 12:40:19 -0800

    Refactor to get base price of pizza, using Pizza class for getTotalPrice method instead of Topping class.

--- js/scripts.js
+++ js/scripts.js
@@ -4,44 +4,75 @@ function Pizza(size, firstTopping) {
   this.toppings = [];
 }
 
-function Topping(pizza, name, type) {
-  this.pizza = pizza;
+function Topping(name, type) {
   this.name = name;
   this.type = type;
+}
+
+Pizza.prototype.getTotalPrice = function() {
+  var basePrice = 0;
+
+  var toppingsPrice = 0;
+
+  var smallPrice = 12;
+  var smallVegetableToppingPrice = 1;
+  var smallMeatToppingPrice = 1.5;
+
+  var mediumPrice = 15;
+  var mediumVegetableToppingPrice = 1.5;
+  var mediumMeatToppingPrice = 2;
+
+  var largePrice = 20;
+  var largeVegetableToppingPrice = 2;
+  var largeMeatToppingPrice = 2.5;
+
+  if (this.size === "Large") {
+      basePrice += largePrice;
+    } else if (this.pizza.size === "Medium") {
+      basePrice += mediumPrice;
+    } else if (this.pizza.size === "Small") {
+      basePrice += smallPrice;
+    }
+
+  // this.toppings.forEach(function(topping) {
+  //
+  // });
+
+  return basePrice;
 }
 
 // TODO: fix bug to calculate price of multiple toppings (Add topping object to toppings array?)
 // CREATE METHOD TO ADD AN ADDITIONAL TOPPING, that goes through getPrice method
 
-Topping.prototype.getAllToppingsPrice = function () {
-  var toppingsPrice = 0;
-
-  var smallVegetableToppingPrice = 1;
-  var smallMeatToppingPrice = 1.5;
-
-  var mediumVegetableToppingPrice = 1.5;
-  var mediumMeatToppingPrice = 2;
-
-  var largeVegetableToppingPrice = 2;
-  var largeMeatToppingPrice = 2.5;
-
-  if (this.type === "Vegetable" && this.pizza.size === "Small") {
-    toppingsPrice += smallVegetableToppingPrice;
-  } else if (this.type === "Meat" && this.pizza.size === "Small") {
-    toppingsPrice += smallMeatToppingPrice;
-  } else if (this.type === "Vegetable" && this.pizza.size === "Medium") {
-    toppingsPrice += mediumVegetableToppingPrice;
-  } else if (this.type === "Meat" && this.pizza.size === "Medium") {
-    toppingsPrice += mediumMeatToppingPrice;
-  } else if (this.type === "Vegetable" && this.pizza.size === "Large") {
-    toppingsPrice += largeVegetableToppingPrice;
-  } else if (this.type === "Meat" && this.pizza.size === "Large") {
-    toppingsPrice += largeMeatToppingPrice;
-  }
-
-  return toppingsPrice;
-
-};
+// Topping.prototype.getAllToppingsPrice = function () {
+//   var toppingsPrice = 0;
+//
+//   var smallVegetableToppingPrice = 1;
+//   var smallMeatToppingPrice = 1.5;
+//
+//   var mediumVegetableToppingPrice = 1.5;
+//   var mediumMeatToppingPrice = 2;
+//
+//   var largeVegetableToppingPrice = 2;
+//   var largeMeatToppingPrice = 2.5;
+//
+//   if (this.type === "Vegetable" && this.pizza.size === "Small") {
+//     toppingsPrice += smallVegetableToppingPrice;
+//   } else if (this.type === "Meat" && this.pizza.size === "Small") {
+//     toppingsPrice += smallMeatToppingPrice;
+//   } else if (this.type === "Vegetable" && this.pizza.size === "Medium") {
+//     toppingsPrice += mediumVegetableToppingPrice;
+//   } else if (this.type === "Meat" && this.pizza.size === "Medium") {
+//     toppingsPrice += mediumMeatToppingPrice;
+//   } else if (this.type === "Vegetable" && this.pizza.size === "Large") {
+//     toppingsPrice += largeVegetableToppingPrice;
+//   } else if (this.type === "Meat" && this.pizza.size === "Large") {
+//     toppingsPrice += largeMeatToppingPrice;
+//   }
+//
+//   return toppingsPrice;
+//
+// };
 
 
 
