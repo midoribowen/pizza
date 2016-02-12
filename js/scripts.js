01de0b4f27b94718389fdafd54799e1f0a2c235e
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-12 10:21:31 -0800

    Pass test to create a new topping with topping properties and the pizza it's going on.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
-function Pizza(size, cost) {
+function Pizza(size) {
   this.size = size;
-  this.cost = cost;
-  this.toppings =[];
+  this.toppings = [];
 }
+
+function Topping(pizza, name, type) {
+  this.pizza = pizza;
+  this.name = name;
+  this.type = type;
+}
+
+// Topping.prototype.getPrice = function() {
+//   var totalPrice = 0;
+//
+//   var smallPrice = 12;
+//   var smallVegetableToppingPrice = 1;
+//   var smallMeatToppingPrice = 1.5;
+//
+//   var mediumPrice = 15;
+//   var mediumVegetableToppingPrice = 1.5;
+//   var mediumMeatToppingPrice = 2;
+//
+//   var largePrice = 20;
+//
+//   if (this.pizza.size === "Large") {
+//     totalPrice += largePrice;
+//   } else if (this.pizza.size === "Medium") {
+//     totalPrice += mediumPrice;
+//   } else if (this.pizza.size === "Small") {
+//     totalPrice += smallPrice;
+//   }
+//
+//   if (this.type === "Vegetable" && this.pizza.size === "Small") {
+//     totalPrice += smallVegetableToppingPrice;
+//   } else if (this.type === "Meat" && this.pizza.size === "Small") {
+//     totalPrice += smallMeatToppingPrice;
+//   }
+//
+//
+//   return totalPrice;
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -5,10 +5,30 @@
 // Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
 
 describe('Pizza', function() {
-  it("creates a pizza object with size and cost properties", function() {
-    var testPizza = new Pizza("Large", 20);
-    expect(testPizza.toppings).to.eql([]);
+  it("creates a pizza object with size and toppings properties", function() {
+    var testPizza = new Pizza("Large");
     expect(testPizza.size).to.equal("Large");
-    expect(testPizza.cost).to.equal(20);
+    expect(testPizza.toppings).to.eql([]);
   });
+});
+
+describe('Topping', function() {
+  it("creates a new topping with topping properties and pizza it's going on", function() {
+    var testPizza = new Pizza("Large");
+    var testTopping = new Topping(testPizza, "mushrooms", "Vegetable");
+    expect(testTopping.pizza).to.equal(testPizza);
+    expect(testTopping.name).to.equal("mushrooms");
+    expect(testTopping.type).to.equal("Vegetable");
+  });
+
+  // it("gets the price of a pizza with sausage", function() {
+  //   var testPizza = new Pizza("Small");
+  //   var toppingTwo = new Topping(testPizza, "sausage", "Meat");
+  //   expect(toppingTwo.getPrice()).to.equal(13.5);
+  // });
+  // it("adds the cost of an additional topping", function() {
+  //   var testPizza = new Pizza("Large");
+  //   var testTopping = new Topping("mushrooms", "vegetable");
+  //   expect(testPizza.getPrice()).to.equal(17);
+  // });
 });
